07275f4c51b76d4c4035845e8e399d8e2b79e4bd
Alex <Alex@BSOFT-105>
2018-11-13 11:04:01 +0530

    dgdfg

--- TestApplication/TestApplication/Form1.Designer.cs
+++ TestApplication/TestApplication/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(137, 97);
+            this.button1.Location = new System.Drawing.Point(74, 101);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
